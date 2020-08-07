c56703b5f38139da65f3daddedb6690e6e4579a7
schalkdaniel <d-schalk@t-online.de>
2020-08-07 14:31:43 +0200

    udpates

--- R/roc_glm.R
+++ R/roc_glm.R
@@ -138,7 +138,7 @@ calcDistrParts = function (formula, data,  w = NULL, params_char)
   if (max(y) == 2) y = y - 1
 
   if (!is.null(w)) {
-    eval(parse(text = "w = ", data, "[['", w, "']]"))
+    eval(parse(text = paste0("w = ", data, "[['", w, "']]")))
   } else {
     w = rep(1, length(y))
   }
